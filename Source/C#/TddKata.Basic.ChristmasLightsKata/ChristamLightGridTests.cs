dca7ca171d84c692d67baaf50cd97a84a783199c
mirojica <miroslavdanic@gmail.com>
2018-02-10 00:03:00 +0100

    Started working on Christmas Lights Kata.

---
+++ Source/C#/TddKata.Basic.ChristmasLightsKata/ChristamLightGridTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices;
+using FluentAssertions;
+using Xunit;
+
+namespace TddKata.Basic.ChristmasLightsKata
+{
+    public class ChristamLightGridTests
+    {
+        [Fact]
+        public void TheLightsAllStartTurnedOff()
+        {
+            var christmasLightGrid = new ChristmasLightsGrid();
+
+            var actualLightsOnNumber = christmasLightGrid.NumberOfLightsOn;
+
+            actualLightsOnNumber.Should().Be(0);
+        }
+
+        [Fact]
+        public void ToggleShouldTurnOnAllOffLightsAndAllTurnOffAllOnLights()
+        {
+            var christmasLightsGrid = new ChristmasLightsGrid();
+
+            christmasLightsGrid.Toggle();
+
+            var actualLightsOnNumber = christmasLightsGrid.NumberOfLightsOn;
+            actualLightsOnNumber.Should().Be(1000000);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 999, 999, 1000000)]
+        [InlineData(0, 0, 2, 2, 9)]
+        [InlineData(20, 10, 2, 2, 171)]
+        [InlineData(13, 100, 13, 150, 51)]
+        [InlineData(17, 112, 17, 112, 1)]
+        public void TurnOnShouldTurnOnLightsOnProvidedCordinates_WhenAllLightsAreOff(int xCordinate1, int yCordinate1, int xCordinate2, int yCordinate2, int expectedNumberOfLightsOn)
+        {
+            var christmasLightsGrid = new ChristmasLightsGrid();
+
+            christmasLightsGrid.TurnOn(xCordinate1, yCordinate1, xCordinate2, yCordinate2);
+
+            var actualLightsOnNumber = christmasLightsGrid.NumberOfLightsOn;
+            actualLightsOnNumber.Should().Be(expectedNumberOfLightsOn);
+        }
+    }
+
+    public class ChristmasLightsGrid
+    {
+        private int _numberOfTurnedOnLights;
+
+        public int NumberOfLightsOn => _numberOfTurnedOnLights;
+
+        public void TurnOn(int xCordinate1, int yCordinate1, int xCordinate2, int yCordinate2)
+        {
+            var xLine = xCordinate1 > xCordinate2 ? xCordinate1 - xCordinate2 + 1 : xCordinate2 - xCordinate1 + 1;
+
+            var yLine = yCordinate1 > yCordinate2 ? yCordinate1 - yCordinate2 + 1 : yCordinate2 - yCordinate1 + 1;
+
+            _numberOfTurnedOnLights = xLine * yLine;
+        }
+
+        public void Toggle()
+        {
+            _numberOfTurnedOnLights = 1000000;
+        }
+    }
+}
---
+++ Source/C#/TddKata.Basic.ChristmasLightsKata/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TddKata.Basic.ChristmasLightsKata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TddKata.Basic.ChristmasLightsKata")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfcc7566-37c5-4a85-aa66-af6087bda276")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

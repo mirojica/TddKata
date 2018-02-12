488e7ee97b76fb0c0f25190d4d912fc15c3df99b
Miroslav Danic <miroslav.danic@devtechgroup.com>
2018-02-12 17:02:47 +0100

    Added more test in Christmas Light Kata.

--- Source/C#/TddKata.Basic.ChristmasLightsKata/ChristamLightGridTests.cs
+++ Source/C#/TddKata.Basic.ChristmasLightsKata/ChristamLightGridTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
@@ -42,26 +43,93 @@ namespace TddKata.Basic.ChristmasLightsKata
             var actualLightsOnNumber = christmasLightsGrid.NumberOfLightsOn;
             actualLightsOnNumber.Should().Be(expectedNumberOfLightsOn);
         }
+
+        [Fact]
+        public void TurnOnShouldTurnOnLightsOnProvidedCordinates_WhenThereIsAlreadyTurnedOnLights()
+        {
+            var christmasLightGrid = new ChristmasLightsGrid();
+            christmasLightGrid.TurnOn(0, 0, 2, 2);
+
+            christmasLightGrid.TurnOn(4, 4, 6, 6);
+
+            var actualLightsOnNumber = christmasLightGrid.NumberOfLightsOn;
+            actualLightsOnNumber.Should().Be(18);
+        }
+
+        [Fact]
+        public void TurnOnShouldTurnOnLightsWhichAreNotAlreadyTurnedOn()
+        {
+            var christmasLightGrid = new ChristmasLightsGrid();
+            christmasLightGrid.TurnOn(7, 14, 3, 12);
+
+            christmasLightGrid.TurnOn(4, 10, 8, 13);
+
+            var actualLightsOnNumber = christmasLightGrid.NumberOfLightsOn;
+            actualLightsOnNumber.Should().Be(27);
+        }
     }
 
     public class ChristmasLightsGrid
     {
-        private int _numberOfTurnedOnLights;
+        private readonly HashSet<ChristamLight> _lightsOn = new HashSet<ChristamLight>();
 
-        public int NumberOfLightsOn => _numberOfTurnedOnLights;
+        public int NumberOfLightsOn { get; private set; }
 
         public void TurnOn(int xCordinate1, int yCordinate1, int xCordinate2, int yCordinate2)
         {
-            var xLine = xCordinate1 > xCordinate2 ? xCordinate1 - xCordinate2 + 1 : xCordinate2 - xCordinate1 + 1;
+            var startingXCordinate = Math.Min(xCordinate2, xCordinate1);
+            var endingXCordinate = Math.Max(xCordinate2, xCordinate1);
 
-            var yLine = yCordinate1 > yCordinate2 ? yCordinate1 - yCordinate2 + 1 : yCordinate2 - yCordinate1 + 1;
+            var startingYCordinate = Math.Min(yCordinate2, yCordinate1);
+            var endingYCordinate = Math.Max(yCordinate2, yCordinate1);
 
-            _numberOfTurnedOnLights = xLine * yLine;
+            for (var i = startingXCordinate; i < endingXCordinate + 1; i++)
+            {
+                for (var j = startingYCordinate; j < endingYCordinate + 1; j++)
+                {
+                    var light = new ChristamLight(i, j);
+
+                    if (!_lightsOn.Contains(light))
+                    {
+                        _lightsOn.Add(light);
+                    }
+                }
+            }
+
+            NumberOfLightsOn = _lightsOn.Count;
         }
 
         public void Toggle()
         {
-            _numberOfTurnedOnLights = 1000000;
+            NumberOfLightsOn = 1000000;
+        }
+    }
+
+    public class ChristamLight
+    {
+        private readonly int _xCordinate;
+        private readonly int _yCordinate;
+
+        public ChristamLight(int xCordinate, int yCordinate)
+        {
+            _xCordinate = xCordinate;
+            _yCordinate = yCordinate;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ChristamLight item))
+            {
+                return false;
+            }
+
+            return _xCordinate.Equals(item._xCordinate) && _yCordinate.Equals(item._yCordinate);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = _xCordinate.GetHashCode();
+            return (hashCode * 397) ^ _yCordinate.GetHashCode();
         }
     }
 }

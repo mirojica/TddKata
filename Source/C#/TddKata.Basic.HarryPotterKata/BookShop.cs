7e2ea21a386143cf56eea26dc1f0840558350ba6
Miroslav Danic <miroslavdanic@gmail.com>
2018-03-18 22:50:48 +0100

    Finished harry potter kata.

---
+++ Source/C#/TddKata.Basic.HarryPotterKata/BookShop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TddKata.Basic.HarryPotterKata
+{
+    public class BookShop
+    {
+        private const int BookPrice = 8;
+
+        private readonly IReadOnlyDictionary<int, int> _discountByNumberOfBooks = new Dictionary<int, int>
+        {
+            {1, 0}, {2, 5}, {3, 10}, {4, 20}, {5, 25}
+        };
+
+        public decimal Sell(params string[] books)
+        {
+            decimal totalPrice = 0;
+
+            var bookAppearance = books.GroupBy(book => book)
+                .ToDictionary(groupOfBooks => groupOfBooks.Key, groupOfBooks => groupOfBooks.Count());
+
+            var maximumBookAppearance = bookAppearance.Values.Max();
+
+            for (var i = 0; i < maximumBookAppearance; i++)
+            {
+                var numberOfBooks = bookAppearance.Count(book => book.Value > i);
+                var priceWithoutDiscount = numberOfBooks * BookPrice;
+                totalPrice += priceWithoutDiscount - Discount(numberOfBooks, priceWithoutDiscount);
+            }
+
+            return totalPrice;
+        }
+
+        private decimal Discount(int numberOfBooks, int priceWithoutDiscount)
+        {
+            return (decimal)(priceWithoutDiscount * _discountByNumberOfBooks[numberOfBooks]) / 100;
+        }
+    }
+}
---
+++ Source/C#/TddKata.Basic.HarryPotterKata/BookShopSellShould.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TddKata.Basic.HarryPotterKata
+{
+    public class BookShopSellShould
+    {
+        [Fact]
+        public void CalculatePriceWithoutDiscount_WhenSellingOneBook()
+        {
+            var bookShop = new BookShop();
+
+            var actualPrice = bookShop.Sell("first book");
+
+            actualPrice.Should().Be(8);
+        }
+
+        [Theory]
+        [InlineData(15.2, "first book", "second book")]
+        [InlineData(21.6, "first book", "second book", "third book")]
+        [InlineData(25.6, "first book", "second book", "third book", "fourth book")]
+        [InlineData(30, "first book", "second book", "third book", "fourth book", "fifth book")]
+        public void CalculatePriceWithApropriateDiscount_WhenSellingMultipleDifferentBooks(decimal expectedPrice, params string[] books)
+        {
+            var bookShop = new BookShop();
+
+            var actualPrice = bookShop.Sell(books);
+
+            actualPrice.Should().Be(expectedPrice);
+        }
+
+        [Fact]
+        public void CalculatePriceWithoutDiscount_WhenAllBooksAreTheSame()
+        {
+            var bookShop = new BookShop();
+
+            var actualPrice = bookShop.Sell("first book", "first book", "first book");
+
+            actualPrice.Should().Be(24);
+        }
+
+        [Theory]
+        [InlineData(23.2, "first book", "second book", "first book")]
+        [InlineData(44.8, "first book", "second book", "first book", "second book", "first book", "third book")]
+        public void CalculatePriceWithApropriateDiscount_WhenBooksAreMixed(decimal expectedPrice, params string[] books)
+        {
+            var bookShop = new BookShop();
+
+            var actualPrice = bookShop.Sell(books);
+
+            actualPrice.Should().Be(expectedPrice);
+        }
+    }
+}
